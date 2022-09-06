3f89a56c270b0ef272ab853e864dd26ead9d3e76
Maris Lorencs <lorencs.maris@gmail.com>
2022-09-06 15:40:38 +0300

    Added few basic movements

--- game.js
+++ game.js
@@ -39,26 +39,151 @@ loadSprite("mushroom", "mushroom.png");
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
 
+  // Constants
+
+  const MOVE_SPEED = 120;
+  const JUMP_FORCE = 380;
+
+  // Level
+
+  let level = 1;
+
   const map = [
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "                               ",
-    "========================= =====",
+    "                                ",
+    "                                ",
+    "                                ",
+    "                                ",
+    "                                ",
+    "  C   =?=C=                     ",
+    "                       $        ",
+    "                       -+       ",
+    "             E    E    []       ",
+    "=========================  =====",
   ];
 
-  const levelConfig = {
-    width: 20,
-    height: 20,
-    "=": () => [sprite("block", solid())],
+  const spriteMap = {
+    "=": () => [sprite("block"), area(), solid()],
+    U: () => [sprite("unboxed-block"), area(), solid()],
+    C: () => [sprite("surprise-block"), area(), solid(), "coin-surprise"],
+    "?": () => [sprite("surprise-block"), area(), solid(), "mushroom-surprise"],
+
+    "-": () => [sprite("tube-top-left"), area(), solid(), scale(0.5)],
+    "+": () => [sprite("tube-top-right"), area(), solid(), scale(0.5)],
+    "[": () => [sprite("tube-bottom-left"), area(), solid(), scale(0.5)],
+    "]": () => [sprite("tube-bottom-right"), area(), solid(), scale(0.5)],
+
+    E: () => [sprite("enemy"), area(), solid()],
+    $: () => [sprite("coin"), area(), solid()],
+    M: () => [sprite("mushroom"), area(), body(), solid(), "mushroom"],
   };
 
+  const levelConfig = {
+    ...spriteMap,
+    width: 20,
+    height: 20,
+  };
+
+  add([text(`level ${level}`), pos(10, 10), scale(0.2)]);
+
   const gameLevel = addLevel(map, levelConfig);
+
+  // Score
+
+  let score = 0;
+
+  const scoreUi = add([
+    text(`Score: ${score}`),
+    pos(10, 25),
+    scale(0.2),
+    layer("ui"),
+    {
+      value: score,
+    },
+  ]);
+
+  // Player
+
+  // Function that controls the size of the character
+  function big() {
+    let timer = 0;
+    let isBig = false;
+
+    return {
+      update() {
+        if (isBig) {
+          timer -= dt();
+
+          if (timer <= 0) {
+            this.smallify();
+          }
+        }
+      },
+      isBig() {
+        return isBig;
+      },
+      smallify() {
+        timer = 0;
+        isBig = false;
+
+        this.scale = vec2(1);
+      },
+      biggify(time) {
+        timer = time;
+        isBig = true;
+
+        this.scale = vec(2);
+      },
+    };
+  }
+
+  const player = add([
+    sprite("maris"),
+    pos(10, 0),
+    area(), // has a collider
+    body(), // responds to physics and gravity
+    big(),
+  ]);
+
+  // Player movement
+
+  onKeyDown("right", () => {
+    player.move(MOVE_SPEED, 0);
+  });
+
+  onKeyDown("left", () => {
+    player.move(-MOVE_SPEED, 0);
+  });
+
+  onKeyPress("up", () => {
+    if (player.isGrounded()) {
+      player.jump(JUMP_FORCE);
+    }
+  });
+
+  // Player events
+
+  player.onHeadbutt((obj) => {
+    if (obj.is("coin-surprise")) {
+      gameLevel.spawn("$", obj.gridPos.sub(0, 1));
+
+      destroy(obj);
+
+      gameLevel.spawn("U", obj.gridPos.sub(0, 0));
+    }
+    if (obj.is("mushroom-surprise")) {
+      gameLevel.spawn("M", obj.gridPos.sub(0, 1));
+
+      destroy(obj);
+
+      gameLevel.spawn("U", obj.gridPos.sub(0, 0));
+    }
+  });
+
+  // Mushroom event
+
+  action("mushroom", (obj) => {
+    obj.move(30, 0);
+  });
 });
 
 // Main function
